df7b938adad5da252ef4f362cc629fa77d19fc81
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2023-10-03 18:43:47 +0800

    moved the big enum file outside

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,126 +1,12 @@
 /*! Parser for the input output command with the user
  */
 
+mod command_list;
 #[cfg(test)]
 mod testing;
+
+use command_list::*;
 //use regex::Regex;
-
-/** Encoding type for the writing and rendering
- */
-#[derive(Debug, Eq, PartialEq)]
-#[allow(unused_variables)]
-#[allow(dead_code)]
-pub enum EncodingType {
-    /// As UTF-8
-    UTF8,
-    /// As UTF-32 Big Endian
-    UTF32,
-    /// As UTF-32 Little Endian
-    UTF32LE,
-}
-
-/** Base number for the printing command
-
-The buffer of the text can be printed in the hex form, or decimal form.
-This enum is a tag for whether to print it as a hex form or decimal.
- */
-#[derive(Debug, Eq, PartialEq)]
-#[allow(unused_variables)]
-#[allow(dead_code)]
-pub enum RawBase {
-    /// Decimal type
-    Dec,
-    /// Hexadecimal type
-    Hex,
-}
-
-/**
-Command entered by the user
-
-A tagged union of the command entered by the user, the corresponding argument,
-and optional information required to execute the command.
-
-Some commands are having optional argument, and the parameter can be obtained
-by asking the user about the parameter.
- */
-#[derive(Debug, Eq, PartialEq)]
-#[allow(unused_variables)]
-#[allow(dead_code)]
-pub enum Commands {
-    /// Quit the program
-    /// # Command
-    /// + `.q`
-    Quit,
-    /// Write the buffer to a file
-    /// # Command
-    /// + `.w <file>` : Write as UTF-8 to *file*.
-    /// + `.w32 <file>` : Write as UTF-32 Big Endian to *file*.
-    /// + `.w32LE <file>` : Write as UTF-32 Little Endian to *file*.
-    Write {
-        enc: EncodingType,
-        file: String,
-    },
-    /// The help page of the program
-    /// Should print all of the available command and the usage
-    /// # Command
-    /// + `.h`
-    /// + `.?`
-    Help,
-    /// Compress the text in the buffer to NFC representation
-    /// # Command
-    /// `.c`
-    Compress,
-    /// Decompress the text in the buffer to NFD representation
-    /// # Command
-    /// `.d`
-    Decompress,
-    /// Append literal unicode codepoint to the buffer
-    /// # Command
-    /// + `.ddd`
-    /// + `.0xnnn`
-    /// + `.xnnn`
-    /// Where `d` is decimal digit, and `n` is hexadecimal digit
-    AppendLit(u32),
-    /// Append the string inserted string to the buffer
-    /// # Note
-    /// all input that is not prefixed with `.` is considered as the raw append string
-    AppendStr(Vec<u32>),
-    InsertLit {
-        pos: u32,
-        chr: u32,
-    },
-    InsertStr {
-        pos: u32,
-        txt: Vec<u32>,
-    },
-    Modify {
-        pos: u32,
-        chr: u32,
-    },
-    /// Kill or delete a character from the stream
-    /// # Note
-    /// All stream following the removed character is pushed forward
-    Kill {
-        pos: u32,
-    },
-    Print(RawBase),
-    /// Purge the buffer to empty it
-    /// # Command
-    /// `.e`
-    Erase,
-    /// Render the buffer to the stdout
-    /// # Note
-    /// defaulted to UTF-8
-    /// # Command
-    /// + `.r` : Render to stdout as UTF-8
-    /// + `.r32` : Render to stdout as UTF-32 Big Endian
-    /// + `.r32LE` : Render to stdout as UTF-32 Little Endian
-    Render(EncodingType),
-    /// Validate the current buffer
-    /// # Command
-    /// `.v`
-    Valid,
-}
 
 /** Parse the user input
 
@@ -146,6 +32,7 @@ fn parse_cmd_dec(inp: std::str::Chars) -> Option<Commands> {
         Err(_) => None,
     }
 }
+
 /** Validator check for the trailing input after the no argument command
 
 The commands without any additional argument should consist of only
@@ -153,7 +40,7 @@ the command and the leading and trailing whitespace.
 Additional character after the command is considered as invalid.
 
 # Applicability
-This check is applicabl to the following commmand:
+This check is valid for the following commmand:
 + `q` Commands::Quit
 + `h` Commands::Help
 + `?` Commands::Help
